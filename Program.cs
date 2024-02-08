0714c77519ad5b2e3b5978bd02e827e973b0be7b
Phuong <lephuong10142002@gmail.com>
2024-02-08 21:04:29 +0700

    install Entity Framework and connect database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using FinShark.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(connection);
+});
 
 var app = builder.Build();
 
