36b2f7420d294931d8abb6833c6c29458bc442b8
Phuong <lephuong10142002@gmail.com>
2024-02-10 21:38:42 +0700

    login, register

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using FinShark.Data;
 using FinShark.Interfaces;
 using FinShark.models;
 using FinShark.Repository;
+using FinShark.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +18,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-builder.Services.AddControllers().AddNewtonsoftJson(options => {
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
 });
 
@@ -27,27 +29,30 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 });
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(
-    options => {
+    options =>
+    {
         options.Password.RequireDigit = true;
         options.Password.RequireLowercase = true;
         options.Password.RequireUppercase = true;
         options.Password.RequireNonAlphanumeric = true;
-        options.Password.RequiredLength = 12;
+        options.Password.RequiredLength = 10;
     }
 )
 .AddEntityFrameworkStores<ApplicationDBContext>();
 
 builder.Services.AddAuthentication(
-    options => {
-        options.DefaultAuthenticateScheme = 
-        options.DefaultChallengeScheme = 
+    options =>
+    {
+        options.DefaultAuthenticateScheme =
+        options.DefaultChallengeScheme =
         options.DefaultForbidScheme =
-        options.DefaultScheme = 
-        options.DefaultSignInScheme = 
+        options.DefaultScheme =
+        options.DefaultSignInScheme =
         options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
     }
 ).AddJwtBearer(
-    options => {
+    options =>
+    {
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
@@ -64,6 +69,7 @@ builder.Services.AddAuthentication(
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
